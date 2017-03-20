87c48adbdee9adbd12d24d1be6f1e5bb028ccf39
Roberto Gardenier <roberto@caroga.net>
2017-03-20 18:53:53 +0100

    Added consumer migration

--- database/migrations/2017_03_20_130310_create_consumers_table.php
+++ database/migrations/2017_03_20_130310_create_consumers_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreateConsumersTable extends Migration
 {
@@ -13,10 +13,20 @@ class CreateConsumersTable extends Migration
      */
     public function up()
     {
-        Schema::create('consumers', function (Blueprint $table) {
-            $table->increments('id');
-            $table->timestamps();
-        });
+        Schema::create(
+            'consumers',
+            function (Blueprint $table) {
+                $table->engine = 'InnoDb';
+
+                $table->uuid('id');
+                $table->string('name');
+                $table->string('url');
+                $table->string('api_token')->nullable();
+                $table->timestamps();
+
+                $table->primary('id');
+            }
+        );
     }
 
     /**
